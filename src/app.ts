6f612b0315afd7320e5f04c1275d006fca500db2
bineetNaidu <bineet.s.naidu@gmail.com>
2021-10-22 23:02:19 +0530

    🚀 add express rate limit

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import express from 'express';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
+import rateLimit from 'express-rate-limit';
 import { ___prod___ } from './utils/contants';
 import { proxyRouter } from './api';
 import { NotFoundError } from './utils/notFoundError';
@@ -12,9 +13,16 @@ import { ExpressErrorHandler } from './utils/ExpressErrorHandler';
 dotenv.config();
 const app = express();
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+});
+
+app.set('trust proxy', 1);
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
+app.use(limiter);
 
 app.use(proxyRouter);
 
